8a538c1d319a219f619cedcc644a4137b429cdee
DigitalExp2 <134752290+OverKira99@users.noreply.github.com>
2026-08-24 14:57:24 -0500

    feat: implement ProductRepository domain abstraction and mock data repository implementation

--- lib/features/shop/domain/repositories/products_repository.dart
+++ lib/features/shop/domain/repositories/products_repository.dart
@@ -2,5 +2,8 @@ import 'package:digi_reto_2/features/shop/domain/entities/products.dart';
 
 abstract class ProductRepository {
   Future<List<Product>> getProducts();
+
   Future<Product> getProduct(int id);
+
+  Future<void> saveProduct(Product product);
 }
